09552736fa156efb9682fa3d1cc018f780a11752
Alberttoto11 <toto.sax.albert@gmail.com>
2025-12-04 16:05:11 +0100

    Elakadas

---
+++ index.js
@@ -0,0 +1,122 @@
+/**
+ * @type {{title:string}}
+ */
+const head_arr=[
+    {
+        title:'Ókóri település'
+    },
+    {
+        title:'Ágazat'
+    },
+    {
+        title:'Példa'
+    }
+]
+
+const body_arr=[
+    {
+        telepules:'Athén',
+        agazat:'politika',
+        agazat2:'tudomány',
+        pelda:'demokrácia',
+        pelda2:'filozófia'
+    },
+    {
+        telepules:'Egyiptom',
+        agazat:'mezőgazdaság',
+        pelda:'csatornák'
+    },
+    {
+        telepules:'Spárta',
+        agazat:'neveléstudomány',
+        agazat2:'harcászat',
+        pelda:'agogé',
+        pelda2:'hoplita'
+    },
+]
+const div=document.createElement('div')
+div.id='jsselcetor'
+div.classList.add('hide')
+document.body.appendChild(div)
+const table=document.createElement('table')
+div.appendChild(table)
+const thead=document.createElement('thead')
+table.appendChild(thead)
+const tr_zero=document.createElement('tr')
+
+for(const key of head_arr){
+    const th=document.createElement('th')
+    th.innerText=key.title
+    tr_zero.appendChild(th)
+}
+thead.appendChild(tr_zero)
+
+const tbody=document.createElement('tbody')
+table.appendChild(tbody)
+for(const key of body_arr){
+    const tr=document.createElement('tr')
+    const tr2=document.createElement('tr')
+    if(key.agazat2 && key.pelda2 !==undefined){
+        const td1=document.createElement('td')
+        const td2=document.createElement('td')
+        const td3=document.createElement('td')
+        const td4=document.createElement('td')
+        const td5=document.createElement('td')
+        
+        td1.rowSpan="2"
+        td1.innerText=key.telepules
+        td2.innerText=key.agazat
+        td3.innerText=key.pelda
+        td4.innerText=key.agazat2
+        td5.innerText=key.pelda2
+        
+        tr.appendChild(td1)
+        tr.appendChild(td2)
+        tr.appendChild(td3)
+
+        tr2.appendChild(td4)
+        tr2.appendChild(td5)
+    }
+    else{
+        const td1=document.createElement('td')
+        const td2=document.createElement('td')
+        const td3=document.createElement('td')
+
+        td1.innerText=key.telepules
+        td2.innerText=key.agazat
+        td3.innerText=key.pelda
+
+        tr.appendChild(td1)
+        tr.appendChild(td2)
+        tr.appendChild(td3)
+    }
+    tbody.appendChild(tr)
+    tbody.appendChild(tr2)
+    
+}
+
+const checkbox=document.getElementById('tableselector')
+checkbox.addEventListener('change',changeCheckbox)
+
+const htmlform=document.getElementById('htmlform')
+htmlform.addEventListener('submit',addEventListener)
+
+
+//-------------Functions-------------
+function hideBasedOnCheckBox(checkbox) {
+    const Check = checkbox.checked;
+    const a = document.getElementById('jsselcetor');
+    const b = document.getElementById('htmlselector');
+
+    if(Check) {
+        a.classList.remove('hide');
+        htmlselector.classList.add('hide');
+    } else {
+        a.classList.add('hide');
+        b.classList.remove('hide');
+    }
+}
+function changeCheckbox(e){
+    const target=e.target
+    hideBasedOnCheckBox(target)
+}
